6c4b9bdccc8aa46e17961a228cc4509e891bf35c
maparisio <meaparisio@gmail.com>
2024-08-17 20:29:57 -0600

    update

--- Leaflet-Part-1/static/logic.js
+++ Leaflet-Part-1/static/logic.js
@@ -48,92 +48,19 @@ function createMap(earthquakes) {
         Earthquakes: earthquakes
     };
 
-    // Create the map with the div and store it in a variable to use it later
-    let myEarthquakeMap = L.map("map", {
+    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    let myMap = L.map("map", {
         center: [37.09, -95.71],
         zoom: 5,
         layers: [street, earthquakes]
     });
 
-    // Create a layer control to pass the baseMaps and overlayMaps
+    // Create a layer control.
+    // Pass it our baseMaps and overlayMaps.
+    // Add the layer control to the map.
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
-    }).addTo(myEarthquakeMap);  // Add the control to the map
+    }).addTo(myMap);
 
-    //legendmap.addTo(myEarthquakeMap); // uncomment to see if your control layer worked
+    }
 
-    //////////////////////////////////////////////////////////////////////////////////
-    /////// 4.2 This section creates the legend for the 'Depth of Earthquake'  ///////
-    /// Make changes to the CSS file accordingly, otherwise it won't display right ///
-    //////////////////////////////////////////////////////////////////////////////////
-
-    // Create a legend for the map, "info legend" is the name for the class of the div
-    let legendmap = L.control({ position: "bottomright" });
-    legendmap.onAdd = function() {
-        let div = L.DomUtil.create("div", "Info Legend");
-
-    // Adding parameters to the legend Info box created above
-    div.innerHTML += '<div class="info legend leaflet-control">' +
-    '<h2 style="text-align: center;">Depth of<br>Earthquake</h2>' +
-    '<div class="legend">' + '<div class="legend-item">' +
-    '<i style="background: green"></i> -10-10' +
-    '</div>' + '<div class="legend-item">' +
-    '<i style="background: yellowgreen"></i> 10-30' +
-    '</div>' + '<div class="legend-item">' +
-    '<i style="background: yellow"></i> 30-50' +
-    '</div>' + '<div class="legend-item">' +
-    '<i style="background: orange"></i> 50-70' +
-    '</div>' + '<div class="legend-item">' +
-    '<i style="background: red"></i> 70-90' +
-    '</div>' + '<div class="legend-item">' +
-    '<i style="background: darkred"></i> 90+' +
-    '</div>' + '</div>' + '</div>';
-
-    return div;
-    }; // End of the legendmap.onAdd function
-
-    //Call in the function we just created above with the legend parameters
-    legendmap.addTo(myEarthquakeMap);
-
-}  // End of the createMap() function
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// 5. Create function to determine the color of the circles for the magnitude
-// for colors use green, yellowgreen, yellow, orange, red, and darkred
-
-function circColor(depth) {
-    if (depth <= 10) return "green";
-    else if(depth > 10 & depth <= 30) return "yellowgreen";
-    else if(depth > 30 & depth <= 50) return "yellow";
-    else if(depth > 50 & depth <= 70) return "orange";
-    else if(depth > 70 & depth <= 90) return "red";
-    else return "darkred";
-};  // End of circColor() function
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// 6. Create function for circle size depending on the magnitude
-
-function circSize(magnitude) {
-    return magnitude * 5.5;
-}; // End of circSize() function
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//7. Create function to make circles for each earthquake feature
-
-function magCircles(feature, latlng) {
-    let depth = feature.geometry.coordinates[2]; // Get depth from the third coordinate
-    let magnitude = feature.properties.mag;
-    let fillColor = circColor(depth);
-    let circleOptions = {
-        radius: circSize(magnitude),
-        fillColor: fillColor,
-        color: "#006400",  // Outline of the circles in darkgreen
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-    };
-    return L.circleMarker(latlng, circleOptions);
-} // End of magCircles() function
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
