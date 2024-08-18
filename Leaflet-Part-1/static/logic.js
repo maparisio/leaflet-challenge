25a13b20b286628e1b20b2f3459658d342df1fce
Michael Aparisio <153468377+maparisio@users.noreply.github.com>
2024-08-17 21:33:36 -0600

    Update logic.js

--- Leaflet-Part-1/static/logic.js
+++ Leaflet-Part-1/static/logic.js
@@ -3,7 +3,7 @@
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL passing a variable function to display the map
-d3.json(queryURL).then(function(data) {
+d3.json(queryUrl).then(function(data) {
    // Once we get a response, send the data.features object to the createFeatures function
     createFeatures(data.features);
 });
@@ -12,7 +12,7 @@ function createFeatures(earthquakeData) {
   // Define a function that we want to run once for each feature in the features array.
   // Give each feature a popup that describes the place and time of the earthquake
     function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr>
+        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
     }
 
     // Create a GeoJSON layer that contains the features array on the earthquakeData object.
@@ -63,4 +63,3 @@ function createMap(earthquakes) {
     }).addTo(myMap);
 
     }
-
